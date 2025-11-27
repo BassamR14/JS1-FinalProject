f4d404bc660b09fa3b9aa2f75f89b7976afd6587
BassamR14 <bassam.ruwaida@gmail.com>
2025-11-27 20:13:28 +0100

    added tooltip to dark mode button + added titles for each segment in html

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 let nameInput = document.querySelector("#name");
 let nameBtn = document.querySelector(".submit-name");
 let intro = document.querySelector(".introduction");
-let toggleBtn = document.querySelector(".toggle");
-let icon = document.querySelector(".material-symbols-outlined");
-let body = document.querySelector("body");
 
 function welcomeMessage() {
   let name = nameInput.value;
@@ -18,22 +15,34 @@ function welcomeMessage() {
 nameBtn.addEventListener("click", welcomeMessage);
 
 //
-// Toggle button changes style of page - dark mode
+// Toggle button changes style of page - dark mode -light mode
+let toggleBtn = document.querySelector(".toggle");
+let icon = document.querySelector(".material-symbols-outlined");
+let body = document.querySelector("body");
+let toolTip = document.querySelector(".tooltiptext");
 let sections = document.querySelectorAll(".question");
 
 function togglePage() {
-  //changes background and text color
+  //changes background and text color of page
   body.classList.toggle("dark-mode");
   //changes button background and text color
   submitBtn.classList.toggle("light-mode");
   nameBtn.classList.toggle("light-mode");
   toggleBtn.classList.toggle("light-mode");
   icon.classList.toggle("light-mode");
+  toolTip.classList.toggle("light-mode");
 
   //to change the border color of the sections
   sections.forEach((section) => {
     section.classList.toggle("white-border");
   });
+
+  //to get the tooltip text to change when toggling dark mode
+  if (body.classList.contains("dark-mode")) {
+    toolTip.textContent = "Light Mode";
+  } else {
+    toolTip.textContent = "Dark Mode";
+  }
 }
 
 toggleBtn.addEventListener("click", togglePage);
@@ -51,21 +60,26 @@ function checkRadioAnswers() {
   let q5Input = document.querySelector("[name=question5]:checked");
   let q6Input = document.querySelector("[name=question6]:checked");
 
-  let radioArray = [
-    q1Input.value,
-    q2Input.value,
-    q3Input.value,
-    q4Input.value,
-    q5Input.value,
-    q6Input.value,
-  ];
+  if (!q1Input || !q2Input || !q3Input || !q4Input || !q5Input || !q6Input) {
+    alert("Answer all the questions please.");
+    return;
+  } else {
+    let radioArray = [
+      q1Input.value,
+      q2Input.value,
+      q3Input.value,
+      q4Input.value,
+      q5Input.value,
+      q6Input.value,
+    ];
 
-  //returns how many correct answers the user has gotten + changes the background color of each section to represent correct/wrong answer.
-  radioArray.forEach((answer) => {
-    if (answer === "correct") {
-      correctAnswers += 1;
-    }
-  });
+    //returns how many correct answers the user has gotten + changes the background color of each section to represent correct/wrong answer.
+    radioArray.forEach((answer) => {
+      if (answer === "correct") {
+        correctAnswers += 1;
+      }
+    });
+  }
 }
 
 //
